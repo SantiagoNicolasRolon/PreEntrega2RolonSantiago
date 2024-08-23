91eb3be6d072ea25ea47b598c835941edf026d84
Santiago Rolon <santi_del95@hotmail.com>
2024-08-22 21:49:39 -0300

    ProyectoFinal-RolonSantiago

--- js/main.js
+++ js/main.js
@@ -1,37 +1,148 @@
+const articulos = document.getElementById("articulos");
+const guardarCarrito = document.getElementById("guardarCarrito");
+const contenedorCarrito = document.getElementById("contenedorCarrito");
+const conteoCarrito = document.getElementById("conteoCarrito");
 
 // INDEX //
 
-const contenedor_articulos = document.querySelector('#articulos');
+const mostrarProductos = (mostrarJoyas) => {
+    articulos.innerHTML = '';
 
-joyas.forEach((Joya) => {
-    const copia = document.querySelector('template').content.cloneNode(true)
+    mostrarJoyas.forEach((Joya) => {
+        const contenido = document.createElement("article");
 
-    copia.querySelector('h2').textContent = Joya.nombre
-    copia.querySelector('img').src = Joya.img
-    copia.querySelector('b').textContent += Joya.precio
+        contenido.innerHTML = `
+        <div class="productos">
+            <h2>${Joya.nombre}</h2>
+            <img src="${Joya.img}">
+            <b>Precio: ${Joya.precio}$</b>
+            <button>Comprar</button>
+        </div>    
+        `;
+        articulos.append(contenido);
 
-    copia.querySelector('button').addEventListener('click', () => {
-        carrito.push(Joya)
-        console.table(carrito)
-    })
+        contenido.querySelector('button').addEventListener('click', () => {
+            const repetir = carrito.some((repetirJoya) => repetirJoya.id === Joya.id);
 
-    contenedor_articulos.append(copia)
-});
-
-// BUSCADOR //
-
-const buscador = document.getElementById("buscador")
-const articulos = document.getElementById("articulos")
-
-const comenzarBusqueda = () => {
-    const buscarJoya = buscador.value.toLowerCase();
-    const filtrarProductos = joyas.filter((joya) => joya.nombre.toLowerCase().startsWith(buscarJoya))
-
-    filtrarProductos.forEach((Joya) => {
-        const template = document.createElement("template");
-        template.textContent = Joya.nombre;
-        articulos.appendChild(template)
-    })
+            if (repetir === true) {
+                carrito.map((J) => {
+                    if (J.id === Joya.id) {
+                        J.stock++;
+                    }
+                });
+            } else {
+                carrito.push({
+                    id: Joya.id,
+                    nombre: Joya.nombre,
+                    precio: Joya.precio,
+                    categoria: Joya.categoria,
+                    img: Joya.img,
+                    stock: Joya.stock,
+                });
+            }
+            cantidadEnCarrito();
+            guardarLocal();
+        });
+    });
 };
 
-buscador.addEventListener("input", comenzarBusqueda);
+
+
+const filtrarJoyas = (categoria) => {
+    const mostrarJoyas = joyas.filter(Joya => Joya.categoria === categoria)
+    mostrarProductos(mostrarJoyas)
+};
+
+const filtroTodo = document.getElementById('filtroTodo');
+const filtroAritos = document.getElementById('filtroAritos');
+const filtroDijes = document.getElementById('filtroDijes');
+const filtroPulceras = document.getElementById('filtroPulceras');
+
+filtroTodo.addEventListener('click', () => {
+    mostrarProductos(joyas)
+});
+filtroAritos.addEventListener('click', () => {
+    filtrarJoyas('aritos')
+});
+filtroDijes.addEventListener('click', () => {
+    filtrarJoyas('dijes')
+});
+filtroPulceras.addEventListener('click', () => {
+    filtrarJoyas('pulceras')
+});
+
+mostrarProductos(joyas)
+
+// CARRITO //
+
+const guardarLocal = () => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+};
+
+const mostrarCarrito = () => {
+    contenedorCarrito.innerHTML = "";
+    contenedorCarrito.style.display = "flex";
+    const carritoHeader = document.createElement("div");
+    carritoHeader.className = "carrito-header"
+    carritoHeader.innerHTML = `
+    <h2>Carrito de compras.</h2>
+    `
+    contenedorCarrito.append(carritoHeader);
+
+    const botonCarrito = document.createElement("p");
+    botonCarrito.innerText = "Cerrar";
+    botonCarrito.addEventListener("click", () => {
+        contenedorCarrito.style.display = "none";
+    })
+
+    carritoHeader.append(botonCarrito);
+
+    carrito.forEach((Carro) => {
+        const contenidoDeCarrito = document.createElement("article")
+        contenidoDeCarrito.className = "carritodecompras"
+        contenidoDeCarrito.innerHTML = `
+            <img src="${Carro.img}">
+            <h2>${Carro.nombre}</h2>
+            <b>Precio: ${Carro.precio}$</b>
+            <p>Unidades: ${Carro.stock}</p>
+            <p>Total: ${Carro.stock * Carro.precio}</p>
+            <span class="quitarprod"> ❌ </span>
+        `
+        contenedorCarrito.append(contenidoDeCarrito);
+
+        const borrar = contenidoDeCarrito.querySelector(".quitarprod");
+
+        borrar.addEventListener("click", () => {
+            borrarProd(Carro.id)
+        });
+    });
+    const total = carrito.reduce((A, E) => A + E.precio * E.stock, 0);
+    const totalApagar = document.createElement("div");
+    totalApagar.className = "totalApagar";
+    totalApagar.innerHTML = `<p>Total a pagar:</p><p>${total} $</p>`;
+    contenedorCarrito.append(totalApagar);
+};
+
+guardarCarrito.addEventListener("click", mostrarCarrito);
+
+const borrarProd = (id) => {
+    const encontrarID = carrito.find((e) => e.id === id);
+
+    carrito = carrito.filter((buscarID) => {
+        return buscarID !== encontrarID;
+    });
+    cantidadEnCarrito();
+    guardarLocal();
+    mostrarCarrito();
+};
+
+const cantidadEnCarrito = () => {
+    conteoCarrito.style.display = "block";
+
+    const carritoCantidad = carrito.length;
+    localStorage.setItem("carritoCantidad", JSON.stringify(carritoCantidad));
+
+    conteoCarrito.innerText = JSON.parse(localStorage.getItem("carritoCantidad"));
+};
+
+cantidadEnCarrito();
--- js/productos.js
+++ js/productos.js
@@ -1,45 +1,32 @@
 class Joya {
-    constructor(nombre, precio, categoria) {
+    constructor(id, nombre, precio, categoria, stock) {
+        this.id = id
         this.nombre = nombre
         this.precio = precio
         this.categoria = categoria
         this.img = `./images/productos/${nombre.toLowerCase()}.jpg`
+        this.stock = stock
     }
 };
 
 const joyas = [
-    new Joya('Abridor Strass Blanco', '10500', 'aritos'),
-    new Joya('Abridor Strass Rojo', '10500', 'aritos'),
-    new Joya('Abridor Strass Verde', '10500', 'aritos'),
-    new Joya('Abridor Strass Rosa', '10500', 'aritos'),
-    new Joya('Abridor Strass Negro', '10500', 'aritos'),
-    new Joya('Abridor Strass Celeste', '10500', 'aritos'),
-    new Joya('Cubano Azul', '14000', 'dijes'),
-    new Joya('Cubano Rojo', '14000', 'dijes'),
-    new Joya('Cubano Rosa', '14000', 'dijes'),
-    new Joya('Cubano Verde', '14000', 'dijes'),
-    new Joya('Cubano Negro', '14000', 'dijes'),
-    new Joya('Cubano Cubic Blanco', '17750', 'dijes'),
-    new Joya('Pulcera Cubic Amarillo', '32000', 'pulceras'),
-    new Joya('Pulcera Cubic Blanco', '32000', 'pulceras'),
-    new Joya('Pulcera Cubic Rosa', '32000', 'pulceras'),
-    new Joya('Pulcera Cubic Violeta', '32000', 'pulceras'),
-    new Joya('Pulcera Bolita de Plata', '33250', 'pulceras')
+    new Joya(1, 'Abridor Strass Blanco', 10500, 'aritos', 1),
+    new Joya(2, 'Abridor Strass Rojo', 10500, 'aritos', 1),
+    new Joya(3, 'Abridor Strass Verde', 10500, 'aritos', 1),
+    new Joya(4, 'Abridor Strass Rosa', 10500, 'aritos', 1),
+    new Joya(5, 'Abridor Strass Negro', 10500, 'aritos', 1),
+    new Joya(6, 'Abridor Strass Celeste', 10500, 'aritos', 1),
+    new Joya(7, 'Cubano Azul', 14000, 'dijes', 1),
+    new Joya(8, 'Cubano Rojo', 14000, 'dijes', 1),
+    new Joya(9, 'Cubano Rosa', 14000, 'dijes', 1),
+    new Joya(10, 'Cubano Verde', 14000, 'dijes', 1),
+    new Joya(11, 'Cubano Negro', 14000, 'dijes', 1),
+    new Joya(12, 'Cubano Cubic Blanco', 17750, 'dijes', 1),
+    new Joya(13, 'Pulcera Cubic Amarillo', 32000, 'pulceras', 1),
+    new Joya(14, 'Pulcera Cubic Blanco', 32000, 'pulceras', 1),
+    new Joya(15, 'Pulcera Cubic Rosa', 32000, 'pulceras', 1),
+    new Joya(16, 'Pulcera Cubic Violeta', 32000, 'pulceras', 1),
+    new Joya(17, 'Pulcera Bolita de Plata', 33250, 'pulceras', 1)
 ];
 
-class Compra {
-    constructor(nombre, precio, categoria) {
-        this.nombre = nombre
-        this.precio = precio
-        this.categoria = categoria
-        this.img = `./images/productos/${nombre.toLowerCase()}.jpg`
-    }
-};
-
-const carrito = [
-
-];
-
-const carrito_JSON = JSON.stringify(carrito);
-
-localStorage.setItem('carrito', carrito_JSON);
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
